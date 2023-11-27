1c891cb906ee4cc1f55c75f2062277462b839cbb
Adwaith <adwaithtry@gmail.com>
2023-11-27 18:18:04 +0530

    new updates(app--user-form)

--- project1/user/views.py
+++ project1/user/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .form import userLogin
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = userLogin(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success
-    form = UserCreationForm()
+            messages.success(request,f"welcome {username}")
+            return redirect('food:Index')
+    form = userLogin()
     return render(request,"users/register.html",{'form':form})
     
